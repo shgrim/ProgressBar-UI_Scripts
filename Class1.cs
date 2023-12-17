df69b2373962d298a19c599b5c7f8a48dd3cfd9b
Raghib Khan <khan03raghib@gmail.com>
2023-12-17 19:24:25 +0530

    Initial commit

---
+++ Class1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBarUI : MonoBehaviour
+{
+    [SerializeField] private GameObject hasprogressgameobject;
+    [SerializeField] private Image baseImage;
+
+    private IHasProgress hasprogress;
+    private void Start()
+    {
+        hasprogress = hasprogressgameobject.GetComponent<IHasProgress>();
+        hasprogress.OnprogressChanged += Hasprogress_OnprogressChanged;
+        baseImage.fillAmount = 0;
+        Hide();
+    }
+
+    private void Hasprogress_OnprogressChanged(object sender, IHasProgress.OnprogressChangedEventargs e)
+    {
+        baseImage.fillAmount = e.progressNormalized;
+        if (e.progressNormalized == 0f || e.progressNormalized == 1f)
+        {
+            Hide();
+        }
+        else { Show(); }
+
+    }
+
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Interface_ProgressBarUI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IHasProgress
+{
+    public event EventHandler<OnprogressChangedEventargs> OnprogressChanged;
+
+    public class OnprogressChangedEventargs : EventArgs
+    {
+        public float progressNormalized;
+    }
+}
